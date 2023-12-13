03db2889917789135544c65c447040ee1e624b4f
diana <diana-safina-2017@mail.ru>
2023-12-13 20:51:22 +0500

    save

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -15,8 +15,26 @@ class Command(BaseCommand):
             for item in data_info:
                 if item["model"] == "catalog.category":
                     category_for_create.append(Category(**item['fields']))
-                else:
-                    product_for_create.append(Product(**item['fields']))
-
         Category.objects.bulk_create(category_for_create)
+
+        product_info = [
+            {"name": "мухомор",
+             "description": "есть нельзя, но он красивый",
+             "image": "",
+             "category": Category(name="грибы", pk=2).save(),
+             "price_one": 70,
+             "date_creation": "2023-12-12T11:35:16Z",
+             "date_last_modification": "2023-12-12T11:35:22Z"},
+
+            {"name": "черешня",
+             "description": "ах какая вкусная",
+             "image": "",
+             "category": Category(name="ягоды", pk=1).save(),
+             "price_one": 7000,
+             "date_creation": "2023-12-12T11:35:16Z",
+             "date_last_modification": "2023-12-12T11:35:22Z"}
+        ]
+        for item_product in product_info:
+            product_for_create.append(Product(**item_product))
+
         Product.objects.bulk_create(product_for_create)
