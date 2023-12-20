c78ac9642c10caf46de829b36b4e70d1ca699f95
diana <diana-safina-2017@mail.ru>
2023-12-21 00:29:22 +0500

    materials

--- materials/urls.py
+++ materials/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from materials.apps import MaterialsConfig
 
 app_name = MaterialsConfig.name
-#from materials.views import MaterialCreateView, MaterialListView
+from materials.views import MaterialCreateView, MaterialListView
 
 urlpatterns = [
-    # path('create/', MaterialCreateView.as_view(), name='create'),
+    path('create/', MaterialCreateView.as_view(), name='create'),
     # path('', MaterialListView.as_view(), name='list'),
     # path('edit/<int:pk>', ..., name='edit'),
     # path('view/<int:pk>', ..., name='view'),
