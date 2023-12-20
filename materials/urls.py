e331c2baf1ae3f2abe9f4d9da49762199cd72049
diana <diana-safina-2017@mail.ru>
2023-12-21 00:25:20 +0500

    materials

--- materials/urls.py
+++ materials/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from materials.apps import MaterialsConfig
 
 app_name = MaterialsConfig.name
-from materials.views import MaterialCreateView, MaterialListView
+#from materials.views import MaterialCreateView, MaterialListView
 
 urlpatterns = [
-    path('create/', MaterialCreateView.as_view(), name='create'),
-    path('', MaterialListView.as_view(), name='list'),
+    # path('create/', MaterialCreateView.as_view(), name='create'),
+    # path('', MaterialListView.as_view(), name='list'),
     # path('edit/<int:pk>', ..., name='edit'),
     # path('view/<int:pk>', ..., name='view'),
     # path('delete/<int:pk>', ..., name='delete')
--- materials/views.py
+++ materials/views.py
@@ -2,11 +2,11 @@ from django.views.generic import CreateView, ListView
 from materials.models import Materials
 from django.urls import reverse_lazy
 
-class MaterialCreateView(CreateView):
-    model = Materials
-    fields = ('title', 'body')
-    success_url = reverse_lazy('materials:list')
-
-class MaterialListView(ListView):
-    model = Materials
+# class MaterialCreateView(CreateView):
+#     model = Materials
+#     fields = ('title', 'body')
+#     success_url = reverse_lazy('materials:list')
+#
+# class MaterialListView(ListView):
+#     model = Materials
 
